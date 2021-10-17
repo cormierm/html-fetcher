00d8836b52379cdf9c3f22503f750d678ebedfc7
Matt Cormier <m.cormier@vehikl.com>
2021-10-17 14:57:40 -0400

    Removes logs from cookies

--- src/cookies.js
+++ src/cookies.js
@@ -1,18 +1,16 @@
 const fs = require('fs').promises;
 
 const restore = async (page, filename) => {
-    console.log('Restoring cookies');
     try {
         const cookiesString = await fs.readFile(`cookies/${filename}.json`);
         const cookies = JSON.parse(cookiesString);
         await page.setCookie(...cookies);
     } catch (err) {
-        console.log(`Could not read ${filename}.json`);
+        console.log(`Could not read cookie ${filename}.json`);
     }
 }
 
 const save = async (page, filename) => {
-    console.log('Saving cookies');
     const cookies = await page.cookies();
     await fs.writeFile(`cookies/${filename}.json`, JSON.stringify(cookies, null, 2));
 }
